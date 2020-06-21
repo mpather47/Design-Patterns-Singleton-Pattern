78e23dea89046ffae51897cb641d55873a72f944
Marcell <mpather47@gmail.com>
2020-06-21 10:38:45 +0200

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+import java.util.LinkedList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        Singleton newInstance = Singleton.getInstance();
+        System.out.println("Instance ID: " + System.identityHashCode(newInstance));
+        System.out.println(newInstance.getLetterList());
+        LinkedList<String> playerOneTiles = newInstance.getTiles(7);
+        System.out.println("Player 1:" + playerOneTiles);
+        System.out.println(newInstance.getLetterList());
+        Singleton instanceTwo = Singleton.getInstance();
+        System.out.println("Instance 2 ID: " + System.identityHashCode(instanceTwo));
+        System.out.println(newInstance.getLetterList());
+        LinkedList<String> playerTwoTiles = newInstance.getTiles(7);
+        System.out.println("Player 2:" + playerTwoTiles);
+    }
+}
